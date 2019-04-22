1e712be5f7bb3c80409f45d7581ce5e7720a76b8
Sergei Kolesin <Sergei_Kolesin@epam.com>
2019-04-22 15:00:01 +0300

    Initial Commit

---
+++ Bindable/Bindable.h
@@ -0,0 +1,19 @@
+//
+//  Bindable.h
+//  Bindable
+//
+//  Created by Sergei Kolesin on 4/22/19.
+//  Copyright © 2019 Sergei Kolesin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Bindable.
+FOUNDATION_EXPORT double BindableVersionNumber;
+
+//! Project version string for Bindable.
+FOUNDATION_EXPORT const unsigned char BindableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bindable/PublicHeader.h>
+
+
